250d6771a5fe22d7cca7b94dc0ba145efa416fb7
KachiLolia <lokachi3000@gmail.com>
2022-06-22 18:31:43 +0100

    first commit

---
+++ message.js
@@ -0,0 +1,11 @@
+var name; 
+name = "Name: Nwaogu Onyekachi Lolia";
+alert(name);
+
+var height; 
+height = " Height: 165cm";
+alert(height);
+
+var country; 
+country = " Country: Nigeria";
+alert(country);
